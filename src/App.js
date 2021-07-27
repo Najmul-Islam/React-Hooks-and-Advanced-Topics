d4347f385b90a962808ef072ae373e474f675afe
Najmul-Islam <najmulislam519@gmail.com>
2021-07-27 18:20:51 +0600

    short-circuit done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
+import React from "react";
+import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <Setup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
